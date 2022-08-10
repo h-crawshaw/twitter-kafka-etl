cc893da008a6326dfbbf44daeb65d683caaa7394
parsifal <crawshawh04@gmail.com>
2022-08-10 14:16:51 +0100

    Added rules-related functions

--- twitter.py
+++ twitter.py
@@ -17,7 +17,7 @@ client = tweepy.Client(bearer_token, api_key, api_key_secret, access_token, acce
 auth = tweepy.OAuth1UserHandler(api_key, api_key_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
-search_terms = ['"data engineer"', 'remote', 'UK']
+search_terms = ['data engineer',  'remote',  'job']
 
 class Listener(tweepy.StreamingClient):
   def on_connect(self):
@@ -25,14 +25,38 @@ class Listener(tweepy.StreamingClient):
 
   def on_tweet(self, tweet):
     if tweet.referenced_tweets == None:
+      print("\n")       #pretty
       print(tweet.text)
-      time.sleep(1)
+      print("------------------------------------------------------------")
+      time.sleep(0.2)
+
 
 stream = Listener(bearer_token=bearer_token)
 
 for term in search_terms:
   stream.add_rules(tweepy.StreamRule(term))
 
-stream.filter(tweet_fields=['referenced_tweets'])
+# stream.filter(tweet_fields=['referenced_tweets'])
 
-  
+
+def get_all_rule_ids():
+  rules = str(stream.get_rules().data)
+  a_list = rules.split(", ")
+
+  subs = "id"
+  ids_str_list = [s for s in a_list if subs in s]
+
+  ids_list = []
+  for string in ids_str_list:
+    ids_list.append(int(''.join(filter(str.isdigit, string))))
+  return ids_list
+
+def delete_all_rules():
+  for id in get_all_rule_ids():
+    stream.delete_rules(ids=id)
+
+# print(stream.get_rules())
+# print("\n")
+# delete_all_rules()
+# print("\n")
+# print(stream.get_rules())
